a569916a38f7c78e2cac67de1cb7742fa19cd79a
a1ienist <138213365+a1ienist@users.noreply.github.com>
2023-12-16 13:01:07 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
-import pytube
-
-from collections import deque
-
 import asyncio
 import discord
 from discord.ext import commands
+from pytube import YouTube
+from collections import deque
 
-queue = []
+queue = deque()
 
 intents = discord.Intents.default()
 intents.members = True
@@ -16,43 +14,37 @@ bot = commands.Bot(command_prefix='/', intents=intents)
 
 @bot.event
 async def on_ready():
-    print(f'Logged in as {bot.user} (ID: {bot.user.id})')
-    print('------')
+    print(f'Logged in as {bot.user} (ID: {bot.user.id})\n------')
 
 async def play_next_track(ctx, delay):
     await asyncio.sleep(delay)
     await skip(ctx)
-   
+
 @bot.command()
-async def play(ctx, query, next_track = False):
-    queue.append(query) if not next_track else None
-    if ctx.voice_client.is_playing():
-        print(queue)
-        return
-        
-    yt = pytube.YouTube(queue[0])
-    print(yt.title)
-    stream = yt.streams.filter(only_audio=True, audio_codec='opus').order_by('abr').desc().first().download(filename='music.webm')
-    print(stream)
-    
-    source = discord.FFmpegOpusAudio(stream)
-    ctx.voice_client.play(source, after=lambda e: print(f'ERROR: {e}') if e else None)
-    await play_next_track(ctx, yt.length)
-        
-    await ctx.send(f'ИГРАЕТ {yt.title}')
+async def play(ctx, query, next_track=False):
+    if not next_track:
+        queue.append(query)
+
+    if not ctx.voice_client.is_playing() and queue:
+        yt = YouTube(queue[0])
+        stream = yt.streams.filter(only_audio=True, audio_codec='opus').order_by('abr').desc().first().download(filename='music.webm')
+        source = discord.FFmpegOpusAudio(stream)
+        ctx.voice_client.play(source, after=lambda e: print(f'ERROR: {e}') if e else None)
+        await play_next_track(ctx, yt.length)
+        await ctx.send(f'ИГРАЕТ {yt.title}')
 
 @bot.command()
 async def skip(ctx):
-    queue.pop(0)
-    print(len(queue))
-    if ctx.voice_client.is_playing():
-        ctx.voice_client.stop()
-    if len(queue):
-        await play(ctx, queue[0], next_track = True)
+    if queue:
+        queue.popleft()
+        if ctx.voice_client.is_playing():
+            ctx.voice_client.stop()
+        if queue:
+            await play(ctx, queue[0], next_track=True)
 
 @bot.command()
 async def q(ctx):
-     await ctx.send(f'ОЧЕРЕДЬ {queue}')
+     await ctx.send(f'ОЧЕРЕДЬ {list(queue)}')
 
 @play.before_invoke
 async def ensure_voice(ctx):
