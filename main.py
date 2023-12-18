83eab000ea50e8a5ab3be39502c11b4c3001a907
teabeattle <54596204+teabeattle@users.noreply.github.com>
2023-12-18 13:36:08 +0300

    niggerlicious implementation of multiserver work

--- main.py
+++ main.py
@@ -5,67 +5,70 @@ from discord.ext import commands
 from pytube import YouTube
 from collections import deque
 
-queue = deque()
-
-
-
-MY_GUILD = discord.Object(id = 0)
-class MyClient(discord.Client):
-    def __init__(self, *, intents: discord.Intents):
-        super().__init__(intents=intents)
-        self.tree = app_commands.CommandTree(self)
-
-    async def setup_hook(self):
-        self.tree.copy_global_to(guild=MY_GUILD)
-        await self.tree.sync(guild=MY_GUILD)
+queue = {}
 
 intents = discord.Intents.all()
-client = MyClient(intents=intents)
+client = discord.Client(intents=intents)
+tree = app_commands.CommandTree(client)
+
 
 @client.event
 async def on_ready():
     print(f'Logged in as {client.user} (ID: {client.user.id})\n------')
+    for guild in client.guilds:
+        queue[guild.id] = deque()
+        tree.copy_global_to(guild = guild)
+        await tree.sync(guild = guild)
+        
+@client.event
+async def on_guild_join(guild):
+    queue[guild.id] = deque()
+    tree.copy_global_to(guild = guild)
+    await tree.sync(guild = guild)
 
-def play_song(voice_client, error = None, new = True):
+
+
+def play_song(guild, error = None, new = True):
     print(f'ERROR: {error}') if error else None
-    queue.popleft() if not new else None
-    if queue:
-        url = queue[0]
+    try:
+        queue[guild.id].popleft() if not new else None
+    except IndexError:
+        pass
+    if queue[guild.id]:
+        url = queue[guild.id][0]
         yt = YouTube(url)
-        stream = yt.streams.filter(only_audio=True, audio_codec='opus').order_by('abr').desc().first().download(filename='music.webm')
+        stream = yt.streams.filter(only_audio=True, audio_codec='opus').order_by('abr').desc().first().download(filename=f'{guild.id}.webm')
         source = discord.FFmpegOpusAudio(stream)
-        voice_client.play(source, after=lambda e: play_song(voice_client, error = e, new = False))
+        guild.voice_client.play(source, after=lambda e: play_song(guild, error = e, new = False))
 
-@client.tree.command(description = 'ПРОПУСКАЕТ ИГРАЮЩИЙ ТРЕК')
+@tree.command(description = 'ПРОПУСКАЕТ ИГРАЮЩИЙ ТРЕК')
 async def skip(interaction: discord.Interaction):
-    if not queue:
+    if not queue[interaction.guild.id]:
         return await interaction.response.send_message(content = 'НЕЧЕГО СКИПАТЬ')
     interaction.guild.voice_client.stop()
     await interaction.response.send_message(content = 'СКИПНУТО')
-    play_song(interaction.guild.voice_client, new = False)
+    play_song(interaction.guild, new = False)
     
             
-@client.tree.command(description = 'ИГРАЕТ МУЗЫКУ')
+@tree.command(description = 'ИГРАЕТ МУЗЫКУ')
 async def play(interaction: discord.Interaction, link: str):
     if interaction.user.voice:
-        if interaction.guild.voice_client:
-            voice_client = interaction.guild.voice_client
-        else:
-            voice_client = await interaction.user.voice.channel.connect()
+        if not interaction.guild.voice_client:
+            await interaction.user.voice.channel.connect()
     else:
-        return await interaction.response.send_message(content = 'ТЫ НЕ В ГОЛОСОВОМ КАНАЛЕ')
+        return await interaction.response.send_message(content = 'ТЫ НЕ В ГОЛОСОВОМ КАНАЛЕ')    
     yt = YouTube(link)
-    queue.append(link)
-    if len(queue) == 1:
+    queue[interaction.guild.id].append(link)
+    if len(queue[interaction.guild.id]) == 1:
         await interaction.response.send_message(content = f'ИГРАЕТ {yt.title}')
-        play_song(voice_client)
+        play_song(interaction.guild)
     else:
         await interaction.response.send_message(content = f'ДОБАВЛЕНО В ОЧЕРЕДЬ {yt.title}')
     
     
 
-@client.tree.command(description = 'ВЫВОДИТ ОЧЕРЕДЬ')
+@tree.command(description = 'ВЫВОДИТ ОЧЕРЕДЬ')
 async def q(interaction: discord.Interaction):
-    await interaction.response.send_message(content = f'ОЧЕРЕДЬ: {queue}')
+    await interaction.response.send_message(content = f'ОЧЕРЕДЬ: {queue[interaction.guild.id]}')
 
 client.run('TOKEN')
