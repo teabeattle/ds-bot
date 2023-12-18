56b0b2cdff2f94e0dd9d2f93abcc08b5bfcd9a6d
teabeattle <54596204+teabeattle@users.noreply.github.com>
2023-12-18 17:37:11 +0300

    make it less niggerlicious

--- main.py
+++ main.py
@@ -5,7 +5,16 @@ from discord.ext import commands
 from pytube import YouTube
 from collections import deque
 
-queue = {}
+guilds = {}
+
+class DServer():
+    def __init__(self, guild):
+        self.guild = guild
+        self.queue = deque()
+    def popleft(self):
+        return self.queue.popleft()
+    def append(self, item):
+        return self.queue.append(item)
 
 intents = discord.Intents.all()
 client = discord.Client(intents=intents)
@@ -16,13 +25,11 @@ tree = app_commands.CommandTree(client)
 async def on_ready():
     print(f'Logged in as {client.user} (ID: {client.user.id})\n------')
     for guild in client.guilds:
-        queue[guild.id] = deque()
         tree.copy_global_to(guild = guild)
         await tree.sync(guild = guild)
         
 @client.event
 async def on_guild_join(guild):
-    queue[guild.id] = deque()
     tree.copy_global_to(guild = guild)
     await tree.sync(guild = guild)
 
@@ -30,24 +37,23 @@ async def on_guild_join(guild):
 
 def play_song(guild, error = None, new = True):
     print(f'ERROR: {error}') if error else None
-    try:
-        queue[guild.id].popleft() if not new else None
-    except IndexError:
-        pass
-    if queue[guild.id]:
-        url = queue[guild.id][0]
+    guilds[guild.id].popleft() if not new else None
+    
+    if guilds[guild.id].queue:
+        url = guilds[guild.id].queue[0]
         yt = YouTube(url)
         stream = yt.streams.filter(only_audio=True, audio_codec='opus').order_by('abr').desc().first().download(filename=f'{guild.id}.webm')
         source = discord.FFmpegOpusAudio(stream)
         guild.voice_client.play(source, after=lambda e: play_song(guild, error = e, new = False))
+        
 
 @tree.command(description = 'ПРОПУСКАЕТ ИГРАЮЩИЙ ТРЕК')
 async def skip(interaction: discord.Interaction):
-    if not queue[interaction.guild.id]:
+    if ((interaction.guild.id not in guilds) or (not guilds[interaction.guild.id].queue)):
         return await interaction.response.send_message(content = 'НЕЧЕГО СКИПАТЬ')
     interaction.guild.voice_client.stop()
     await interaction.response.send_message(content = 'СКИПНУТО')
-    play_song(interaction.guild, new = False)
+    play_song(interaction.guild)
     
             
 @tree.command(description = 'ИГРАЕТ МУЗЫКУ')
@@ -56,10 +62,12 @@ async def play(interaction: discord.Interaction, link: str):
         if not interaction.guild.voice_client:
             await interaction.user.voice.channel.connect()
     else:
-        return await interaction.response.send_message(content = 'ТЫ НЕ В ГОЛОСОВОМ КАНАЛЕ')    
+        return await interaction.response.send_message(content = 'ТЫ НЕ В ГОЛОСОВОМ КАНАЛЕ')
+    guilds[interaction.guild.id] = DServer(interaction.guild) if interaction.guild.id not in guilds else guilds[interaction.guild.id]
     yt = YouTube(link)
-    queue[interaction.guild.id].append(link)
-    if len(queue[interaction.guild.id]) == 1:
+    guilds[interaction.guild.id].append(link)
+        
+    if len(guilds[interaction.guild.id].queue) == 1:
         await interaction.response.send_message(content = f'ИГРАЕТ {yt.title}')
         play_song(interaction.guild)
     else:
@@ -69,6 +77,6 @@ async def play(interaction: discord.Interaction, link: str):
 
 @tree.command(description = 'ВЫВОДИТ ОЧЕРЕДЬ')
 async def q(interaction: discord.Interaction):
-    await interaction.response.send_message(content = f'ОЧЕРЕДЬ: {queue[interaction.guild.id]}')
+    await interaction.response.send_message(content = f'ОЧЕРЕДЬ: {guilds[interaction.guild.id].queue}')
 
 client.run('TOKEN')
